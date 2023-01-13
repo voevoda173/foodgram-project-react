e65a216cc73ba7d1e80a93a3766d824789e94741
Алексей Воеводин <voevodinal173@gmail.com>
2023-01-13 20:29:36 +0300

    fix load_db

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -49,7 +49,6 @@ class Ingredient(models.Model):
     name = models.CharField(
         verbose_name='Название ингредиента',
         max_length=200,
-        unique=True,
         null=False,
         help_text='Введите название ингредиента',
     )
