02dcae73ab187bd55c900d6831c9221f3bf26bcc
Алексей Воеводин <voevodinal173@gmail.com>
2023-01-13 14:02:59 +0300

    after second review + deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.passworvalidation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
