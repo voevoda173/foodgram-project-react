e10b6c1a46aca11fb8733b02b10fb9ca0e0199e8
Alexey Voevodin <104690441+voevoda173@users.noreply.github.com>
2023-01-14 11:27:59 +0300

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -18,9 +18,9 @@ class IsAuthorOrStaff(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in ('GET', )
+            request.method in permissions.SAFE_METHODS
+            or request.user.role in ["admin"]
             or obj.author == request.user
-            or request.user.is_admin
         )
 
 
